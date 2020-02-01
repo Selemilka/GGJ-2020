aefd97078cce29befa3456b1b09c6ea850a8e667
Sergey Mozgovoy <serg120401@gmail.com>
2020-02-01 17:00:52 +0300

    WTF

---
+++ Assets/Resourсes/Scripts/OpenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Transform PlayerPoint, CameraPoint;
+    public Camera Camera;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.transform.position = PlayerPoint.position;
+            Camera.transform.position = CameraPoint.position;
+        }
+    }
+}
