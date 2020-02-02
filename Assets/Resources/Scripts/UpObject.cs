9ad67ee0fc5d329fc84816480b97b7a7dc766136
Сергей Фомин <sdfomin@edu.hse.ru>
2020-02-02 16:01:20 +0300

    subtitles

--- Assets/Resources/Scripts/UpObject.cs
+++ Assets/Resources/Scripts/UpObject.cs
@@ -13,24 +13,28 @@ public class UpObject : MonoBehaviour
             Debug.Log("something happened");
             collision.GetComponent<Animator>().SetBool("HaveBox", true);
             Destroy(Object);
+            SubtitesManager.Instance.ShowSubtitle("подняли коробку", 2000);
         }
         if (Input.GetKeyUp(KeyCode.Space) && collision.GetComponent<Animator>().GetBool("HaveH") == false && Object.tag == "Hummer")
         {
             Debug.Log("something happened");
             collision.GetComponent<Animator>().SetBool("HaveH", true);
             Destroy(Object);
+            SubtitesManager.Instance.ShowSubtitle("подняли коробку", 2000);
         }
         if (Input.GetKeyUp(KeyCode.Space) && collision.GetComponent<Animator>().GetBool("HaveBH") == false && Object.tag == "BigHummer")
         {
             Debug.Log("something happened");
             collision.GetComponent<Animator>().SetBool("HaveBH", true);
             Destroy(Object);
+            SubtitesManager.Instance.ShowSubtitle("подняли коробку", 2000);
         }
         if (Input.GetKeyUp(KeyCode.Space) && collision.GetComponent<Animator>().GetBool("HaveTape") == false && Object.tag == "Tape")
         {
             Debug.Log("something happened");
             collision.GetComponent<Animator>().SetBool("HaveTape", true);
             Destroy(Object);
+            SubtitesManager.Instance.ShowSubtitle("подняли коробку", 2000);
         }
     }
 }
