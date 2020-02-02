e9661f72e69526758ec994f01fe4f11d86848630
Сергей Фомин <sdfomin@edu.hse.ru>
2020-02-02 16:47:16 +0300

    video

---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        GameObject camera = GameObject.Find("Main Camera");
+
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+        videoPlayer.playOnAwake = false;
+    }
+}
